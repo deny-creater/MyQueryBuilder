e73203fc8286b840659c596f498e3c0ce7dd6573
deny-creator <angrysmd@gmail.com>
2015-12-12 01:35:49 +0400

    Create config_example.php

---
+++ config_example.php
@@ -0,0 +1,11 @@
+<?php
+
+$config = array(
+    'type'=>'mysql',
+    'user'=>'root',
+    'pass'=>'',
+    'host'=>'localhost', 
+    'dbname'=>'MyQueryBuilder'
+);
+
+$db = new MyQueryBuilder($config);
